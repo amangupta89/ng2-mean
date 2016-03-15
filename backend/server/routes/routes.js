a6ddb42d09ee1ac8f089862c559bebe1da96f1e7
Preston Lamb <preston.j.lamb@gmail.com>
2016-03-15 15:37:29 -0600

    -m added a test api endpoint

--- backend/server/routes/routes.js
+++ backend/server/routes/routes.js
@@ -4,4 +4,23 @@ module.exports = function(app) {
 	app.get('/', function(req, res) {
 		res.render('index');
 	});
+
+	app.get('/api/test', function(req, res) {
+		var data = [
+			{
+				name: 'Preston',
+				age: 27
+			},
+			{
+				name: 'Amanda',
+				age: 27
+			},
+			{
+				name: 'Emma',
+				age: 3
+			}
+		];
+		console.log('/api/test: ', data);
+		res.send(data)
+	});
 }
--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -1,19 +1,25 @@
 import {Component} from 'angular2/core';
+import {Observable} from 'rxjs/Observable';
+
+import {TestData} from '../../services/test-data';
 
 @Component({
   selector: 'home',
   templateUrl: 'app/components/home/home.html',
   styleUrls: ['app/components/home/home.css'],
-  providers: [],
+  providers: [TestData],
   directives: [],
   pipes: []
 })
 export class Home {
+	public data: Observable<any>;
 
-  constructor() {}
+	constructor(private testData:TestData) {}
 
-  ngOnInit() {
-
-  }
+	ngOnInit() {
+		this.testData.getTestData().subscribe(
+			data => { this.data = data; console.log('this.data: ', this.data);}
+		);
+	}
 
 }
