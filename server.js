9cfbf5421d575bc0fa1230c5e4e48cecec02f1da
Preston Lamb <preston.j.lamb@gmail.com>
2016-03-14 23:39:03 -0600

    added mongoose and set up the database connection

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var port = process.env.PORT || 5000;
 
+var config = require('./server/config/config')[env];
+
+require('./server/config/mongoose')(config);
+
 app.use(express.static(__dirname + '/server/public'));
 app.set('views', './server/views');
 app.set('view engine', 'jade');
---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	development: {
+		db: 'mongodb://localhost/ng2-mean'
+	},
+	production: {
+		db: 'mongodb://localhost/ng2-mean'
+	}
+}
